9fdb41c2fc9aefee4cf2c303cdc427097ff99ffa
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-06-19 13:04:54 -0600

    Mobile style

--- src/js/app.js
+++ src/js/app.js
@@ -21,20 +21,20 @@ function render ()
 {
     document.querySelector ('.cmp').innerHTML += data.map (obj => 
         `
-        <div class="cmp__block cmp__block-${obj.title}">
+        <div class="cmp__block cmp__block-${obj.title.replace (' ', '-')}">
             <div class="cmp__block__inner">
-                <div class="cmp__header">
-                    <h3 class="cmp__title">${obj.title}</h3>
-                    <img src="./images/icon-ellipsis.svg" alt="Ellipsis" class="cmp__ellipsis"/>
+                <div class="cmp__block__header">
+                    <h3 class="cmp__block__title">${obj.title}</h3>
+                    <img src="./images/icon-ellipsis.svg" alt="Ellipsis" class="cmp__block__ellipsis"/>
                 </div>
-                <div class ="cmp__main">
-                    <h2 class="cmp__time">${obj.timeframes[opt].current}hrs</h2>
-                    <p class="cmp__time-prev">${opt === 'daily' ? 'Yesterday' : opt === 'weekly' ? 'Last Week' : 'Last Month'} - ${obj.timeframes[opt].previous}hrs</p>
+                <div class ="cmp__block__main">
+                    <h2 class="cmp__block__time">${obj.timeframes[opt].current}hrs</h2>
+                    <p class="cmp__block__time-prev">${opt === 'daily' ? 'Yesterday' : opt === 'weekly' ? 'Last Week' : 'Last Month'} - ${obj.timeframes[opt].previous}hrs</p>
                 </div>
             </div>
         </div>
         `
-    )
+    ).join ('');
 }
 
 
